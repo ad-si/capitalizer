edf7fbb49be45baaf47c47b6c1f29d9e812bc8e0
Adrian Sieber <mail@adriansieber.com>
2015-02-21 18:49:44 +0100

    Add failing tests

---
+++ test/index.js
@@ -0,0 +1,50 @@
+var assert = require('assert'),
+	capitalize = require('../index.js')
+
+
+describe('Capitalizer', function () {
+
+	it('Capitalizes the first letter of a string', function () {
+		assert.equal(capitalize('test'), 'Test')
+		assert.equal(capitalize.first('test'), 'Test')
+	})
+
+
+	it('Capitalizes all words in a string', function () {
+		assert.equal(
+			capitalize.all('this is a test sentence'),
+			'This Is A Test Sentence'
+		)
+	})
+
+
+	it('Capitalizes all words in an array', function () {
+		assert.equal(
+			capitalize(['this', 'is', 'a', 'test', 'sentence']),
+			['This', 'Is', 'A', 'Test', 'Sentence']
+		)
+	})
+
+
+	it.skip('Recursively capitalizes all strings in an object', function () {
+		assert.equal(
+			capitalize({
+				name: 'john',
+				hobby: 'chillin',
+				address: {
+					country: 'australia',
+					street: 'wayne boulevard'
+				}
+			}),
+			{
+				name: 'John',
+				hobby: 'Chillin',
+				address: {
+					country: 'Australia',
+					street: 'Wayne boulevard'
+				}
+			}
+		)
+	})
+})
+
