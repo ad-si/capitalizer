f88afd47e20c6986841e2386743e9030d2b94eff
Adrian Sieber <mail@adriansieber.com>
2025-10-27 19:15:26 +0000

    Support capitalizing all value strings in an object

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-type CapitalizerInput = string | string[];
+type CapitalizerInput = string | string[] | Record<string, any>;
 
 interface CapitalizerFunction {
-  (value: CapitalizerInput): string | string[] | undefined;
+  (value: CapitalizerInput): string | string[] | Record<string, any> | undefined;
   first: (value: string) => string;
-  all: (value: CapitalizerInput) => string | string[] | undefined;
+  all: (value: CapitalizerInput) => string | string[] | Record<string, any> | undefined;
 }
 
 function capitalizeString(str: string): string {
@@ -14,13 +14,40 @@ function capitalizeArray(array: string[]): string[] {
 	return array.map((str: string) => capitalizeString(str));
 }
 
-function capitalizer(value: CapitalizerInput): string | string[] | undefined {
+function capitalizeObject(obj: Record<string, any>): Record<string, any> {
+	const result: Record<string, any> = {};
+
+	for (const key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			const value = obj[key];
+
+			if (typeof value === 'string') {
+				// Capitalize all words in the string (like capitalize.all)
+				result[key] = capitalizeArray(value.split(' ')).join(' ');
+			}
+			else if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+				result[key] = capitalizeObject(value);
+			}
+			else {
+				result[key] = value;
+			}
+		}
+	}
+
+	return result;
+}
+
+function capitalizer(value: CapitalizerInput): string | string[] | Record<string, any> | undefined {
 	if (typeof value === 'string') {
 		return capitalizeString(value);
 	}
 
 	if (Array.isArray(value)) {
 		return capitalizeArray(value);
+	}
+
+	if (typeof value === 'object' && value !== null) {
+		return capitalizeObject(value);
 	}
 }
 
